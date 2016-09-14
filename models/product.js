3e5aece5b338f16bcc0052dcfe1796e6e574b50f
aryan singh <aryan@iitgn.ac.in>
2016-09-15 00:05:22 +0530

    several untracked files

---
+++ models/product.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var mongoosastic = require('mongoosastic');
+var Schema = mongoose.Schema;
+
+var ProductSchema = new Schema({
+  category: { type: Schema.ObjectId, ref: 'Category'},
+  name: String,
+  price: Number,
+  image: String
+});
+ProductSchema.plugin(mongoosastic,{
+  hosts: [
+    'localhost:9200'
+  ]
+});
+module.exports = mongoose.model('Product', ProductSchema);
